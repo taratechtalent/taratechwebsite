3c8313b801f893954225ce782c750f9aa92ca154
Mohamed Ahmadian <m.ahmadian@oxolo.com>
2023-12-25 18:53:15 +0330

    add some contact btn

--- frontend/app/service/page.tsx
+++ frontend/app/service/page.tsx
@@ -42,261 +42,166 @@ export default function Help() {
             desc="Best solutions for your bussiness with amazing talent"
           />
         </div>
+
+        <div className="row p-4 m-4" style={{ backgroundColor: "#ff000017" }}>
+          <div className="col-sm-12 text-center">
+            <Link
+              href="/book"
+              className="btn r-04 btn--theme hover--theme mb-20  mt-20"
+            >
+              Team up for success
+            </Link>
+          </div>
+        </div>
         <hr className="divider" />
 
-        <section
-          id="faqs-4"
-          className="gr--whitesmoke inner-page-hero pb-100 faqs-section division !pt-10"
-        >
+        <section id="faqs-3" className="gr--whitesmoke pt-100 faqs-section">
           <div className="container">
             {/* SECTION TITLE */}
             <div className="row justify-content-center">
-              <div className="col-lg-11 col-xl-10">
-                <div className="inner-page-title">
-                  <h2 className="s-52 w-700">FAQ about our solution</h2>
-                </div>
-                <div className="row justify-content-center">
-                  <div className="col-xl-10">
-                    <div className="accordion-wrapper">
-                      <ul className="accordion">
-                        {/* QUESTION #1 */}
-                        <li
-                          className={
-                            isActive.key == 1
-                              ? "accordion-item is-active"
-                              : "accordion-item"
-                          }
-                          onClick={() => handleToggle(1)}
-                        >
-                          {/* Question */}
-                          <div className="accordion-thumb">
-                            <h5 className="s-22 w-700">
-                              What is Martex and how does it work?
-                            </h5>
-                          </div>
-                          {/* Answer */}
-                          <div
-                            className="accordion-panel color--grey"
-                            style={{
-                              display: `${
-                                isActive.key == 1 ? "block" : "none"
-                              }`,
-                            }}
-                          >
-                            {/* Text */}
-                            <p>
-                              Sagittis congue augue egestas volutpat egestas
-                              magna suscipit egestas magna ipsum vitae purus
-                              congue efficitur and ipsum primis in cubilia
-                              laoreet augue egestas luctus donec and curabitur
-                              dapibus
-                            </p>
-                          </div>
-                        </li>{" "}
-                        {/* END QUESTION #1 */}
-                        {/* QUESTION #2 */}
-                        <li
-                          className={
-                            isActive.key == 2
-                              ? "accordion-item is-active"
-                              : "accordion-item"
-                          }
-                          onClick={() => handleToggle(2)}
-                        >
-                          {/* Question */}
-                          <div className="accordion-thumb">
-                            <h5 className="s-22 w-700">
-                              {` What's`} inside the package?
-                            </h5>
-                          </div>
-                          {/* Answer */}
-                          <div
-                            className="accordion-panel color--grey"
-                            style={{
-                              display: `${
-                                isActive.key == 2 ? "block" : "none"
-                              }`,
-                            }}
-                          >
-                            {/* Text */}
-                            <p>
-                              Sagittis congue augue egestas volutpat egestas
-                              magna suscipit egestas tempor magna undo ipsum
-                              vitae purus and efficitur ipsum primis in cubilia
-                              laoreet tempor gravida luctus neque purus ipsum
-                              neque
-                            </p>
-                            {/* Text */}
-                            <p>
-                              Sapien egestas, congue gestas posuere cubilia
-                              congue ipsum mauris lectus laoreet and gestas
-                              neque vitae auctor dolor luctus placerat a magna
-                              cursus congue magna mpedit ligula undo congue.
-                              Maecenas agravida augue porttitor nunc, quis
-                              vehicula magna luctus tempor. Quisque vel laoreet
-                              turpis. Viverra augue, a augue tempor, dictum
-                              tempor diam. Sed pulvinar a consectetur nibh,
-                              imperdiet varius viverra
-                            </p>
-                          </div>
-                        </li>{" "}
-                        {/* END QUESTION #2 */}
-                        {/* QUESTION #3 */}
-                        <li
-                          className={
-                            isActive.key == 3
-                              ? "accordion-item is-active"
-                              : "accordion-item"
-                          }
-                          onClick={() => handleToggle(3)}
-                        >
-                          {/* Question */}
-                          <div className="accordion-thumb">
-                            <h5 className="s-22 w-700">General settings</h5>
-                          </div>
-                          {/* Answer */}
-                          <div
-                            className="accordion-panel color--grey"
-                            style={{
-                              display: `${
-                                isActive.key == 3 ? "block" : "none"
-                              }`,
-                            }}
-                          >
-                            {/* Text */}
-                            <p>
-                              An augue cubilia laoreet magna suscipit egestas
-                              magna ipsum purus ipsum a primis an augue ultrice
-                              ligula egestas suscipit lectus gestas integer
-                              congue a lectus porta phasellus neque blandit
-                              tristique
-                            </p>
-                            {/* Text */}
-                            <p>
-                              Sagittis congue augue egestas volutpat egestas
-                              magna suscipit egestas and magna and vehicula
-                            </p>
-                            {/* Text */}
-                            <p>
-                              Sagittis congue augue egestas volutpat egestas
-                              magna suscipit lipsum egestas magna ipsum vitae a
-                              purus ipsum congue efficitur ipsum primis in
-                              cubilia laoreet augue egestas luctus donec and
-                              curabitur
-                            </p>
-                          </div>
-                        </li>{" "}
-                        {/* END QUESTION #3 */}
-                        {/* QUESTION #4 */}
-                        <li
-                          className={
-                            isActive.key == 4
-                              ? "accordion-item is-active"
-                              : "accordion-item"
-                          }
-                          onClick={() => handleToggle(4)}
-                        >
-                          {/* Question */}
-                          <div className="accordion-thumb">
-                            <h5 className="s-22 w-700">
-                              Which languages does Martex support?
-                            </h5>
-                          </div>
-                          {/* Answer */}
-                          <div
-                            className="accordion-panel color--grey"
-                            style={{
-                              display: `${
-                                isActive.key == 4 ? "block" : "none"
-                              }`,
-                            }}
-                          >
-                            {/* Text */}
-                            <p>
-                              An augue cubilia laoreet and magna suscipit
-                              egestas magna ipsum purus ipsum primis undo augue
-                              ultrice ligula egestas suscipit lectus gestas
-                              integer congue a lectus porta tristique phasellus
-                              neque a blandit and tristique justo aliquam justo
-                              suscipit congue augue egestas volutpat egestas
-                              magna sem congue
-                            </p>
-                            {/* Text */}
-                            <p>
-                              An augue cubilia laoreet and magna suscipit
-                              egestas magna ipsum purus ipsum primis and augue
-                              efficitur ligula egestas suscipit lectus gestas
-                              integer congue a lectus porta phasellus neque
-                            </p>
-                          </div>
-                        </li>{" "}
-                        {/* END QUESTION #4 */}
-                        {/* QUESTION #5 */}
-                        <li
-                          className={
-                            isActive.key == 5
-                              ? "accordion-item acc-last-item is-active"
-                              : "accordion-item acc-last-item"
-                          }
-                          onClick={() => handleToggle(5)}
-                        >
-                          {/* Question */}
-                          <div className="accordion-thumb">
-                            <h5 className="s-22 w-700">
-                              Automate testing with API
-                            </h5>
-                          </div>
-                          {/* Answer */}
-                          <div
-                            className="accordion-panel color--grey"
-                            style={{
-                              display: `${
-                                isActive.key == 5 ? "block" : "none"
-                              }`,
-                            }}
-                          >
-                            {/* Text */}
-                            <ul className="simple-list">
-                              <li className="list-item">
-                                <p>
-                                  Curabitur ac dapibus libero quisque eu congue
-                                  tristique egestas phasellus blandit tristique
-                                  justo aliquam. Aliquam vitae molestie quisque
-                                  sapien justo, aliquet aliquam molestie sed
-                                  purus undo venenatis tempor gravida eget
-                                  lacinia. Augue aliquam a suscipit tincidunt
-                                  tincidunt massa
-                                </p>
-                              </li>
-                              <li className="list-item">
-                                <p>
-                                  Aliquam vitae molestie nunc. Quisque sapien
-                                  justo, aliquet non molestie sed purus,
-                                  venenatis
-                                </p>
-                              </li>
-                              <li className="list-item">
-                                <p>
-                                  Sagittis congue augue egestas volutpat egestas
-                                  magna suscipit egestas magna ipsum vitae an
-                                  efficitur purus undo ipsum primis in cubilia
-                                  laoreet augue egestas luctus donec curabitur
-                                  dapibus libero
-                                </p>
-                              </li>
-                            </ul>
-                          </div>
-                        </li>{" "}
-                        {/* END QUESTION #5 */}
-                      </ul>
-                    </div>
-                  </div>
+              <div className="col-md-10 col-lg-9">
+                <div className="section-title mb-70">
+                  {/* Title */}
+                  <h2 className="s-52 w-700">Our Unique solutions FAQ</h2>
+                  {/* Text */}
+                  <p className="s-21 color--grey">
+                    Most important questions maybe help you better to work with
+                    us
+                  </p>
                 </div>
               </div>
             </div>
-          </div>
+            {/* FAQs-3 QUESTIONS */}
+            <div className="faqs-3-questions">
+              <div className="row">
+                {/* QUESTIONS HOLDER */}
+                <div className="col-lg-6">
+                  <div className="questions-holder">
+                    {/* QUESTION #1 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>1.</span> Getting started with Martex
+                      </h5>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        Etiam amet mauris suscipit in odio integer congue metus
+                        and vitae arcu mollis blandit ultrice ligula egestas
+                        magna suscipit lectus magna suscipit luctus blandit and
+                        laoreet
+                      </p>
+                    </div>
+                    {/* QUESTION #2 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>2.</span> How do I choose a plan?
+                      </h5>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        An enim nullam tempor sapien gravida donec ipsum and
+                        enim porta justo integer at velna vitae auctor integer
+                        congue undo magna laoreet augue pretium purus pretium
+                        ligula
+                      </p>
+                    </div>
+                    {/* QUESTION #3 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>3.</span> Do I need a credit card to sign up?
+                      </h5>
+                      {/* Answer */}
+                      <ul className="simple-list color--grey">
+                        <li className="list-item">
+                          <p>
+                            Fringilla risus, luctus mauris orci auctor purus
+                            ligula euismod pretium purus pretium rutrum tempor
+                            sapien
+                          </p>
+                        </li>
+                        <li className="list-item">
+                          <p>
+                            Nemo ipsam egestas volute undo turpis purus lipsum
+                            primis aliquam sapien quaerat sodales pretium a
+                            purus
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>{" "}
+                {/* END QUESTIONS HOLDER */}
+                {/* QUESTIONS WRAPPER */}
+                <div className="col-lg-6">
+                  <div className="questions-holder">
+                    {/* QUESTION #4 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>4.</span> Discounts for annual plans
+                      </h5>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        Quaerat sodales sapien euismod blandit purus a purus
+                        ipsum primis in cubilia laoreet augue luctus dolor
+                        luctus
+                      </p>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        An enim nullam tempor sapien gravida donec congue metus.
+                        Vitae arcu mollis blandit integer nemo volute velna
+                      </p>
+                    </div>
+                    {/* QUESTION #5 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>5.</span> How to request a refund?
+                      </h5>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        Cubilia laoreet augue egestas and luctus donec curabite
+                        diam vitae dapibus libero and quisque gravida donec
+                        neque blandit justo aliquam molestie nunc sapien justo
+                      </p>
+                    </div>
+                    {/* QUESTION #6 */}
+                    <div className="question mb-35 wow fadeInUp">
+                      {/* Question */}
+                      <h5 className="s-22 w-700">
+                        <span>6.</span> Can I cancel at anytime?
+                      </h5>
+                      {/* Answer */}
+                      <p className="color--grey">
+                        An enim nullam tempor sapien gravida donec ipsum and
+                        enim porta justo integer at velna vitae auctor integer
+                        congue undo magna laoreet augue pretium purus pretium
+                        ligula
+                      </p>
+                    </div>
+                  </div>
+                </div>{" "}
+                {/* END QUESTIONS HOLDER */}
+              </div>{" "}
+              {/* End row */}
+            </div>{" "}
+            {/* END FAQs-3 QUESTIONS */}
+            {/* MORE QUESTIONS LINK */}
+          </div>{" "}
+          {/* End container */}
         </section>
 
+        <div className="row p-4 m-4" style={{ backgroundColor: "#ff000017" }}>
+          <div className="col-sm-12 text-center">
+            <Link
+              href="/book"
+              className="btn r-04 btn--theme hover--theme mb-20  mt-20"
+            >
+              Team up for success
+            </Link>
+          </div>
+        </div>
         <hr className="divider" />
 
         <section className="py-100 ct-02 content-section division">
