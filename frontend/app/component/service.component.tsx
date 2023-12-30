bb15308cb00973ee323ad104e115d2f7736a2e47
Mohamed Ahmadian <m.ahmadian@oxolo.com>
2023-12-30 15:17:19 +0330

    services changes

--- frontend/app/component/service.component.tsx
+++ frontend/app/component/service.component.tsx
@@ -39,7 +39,7 @@ const ServiceComponent = ({
       <div className="fbox-wrapper">
         <div className="row row-cols-1 row-cols-md-2 rows-3">
           {/* FEATURE BOX #1 */}
-          <div className="col">
+          <div className="col  shadow-box">
             <div className="fbox-11 fb-1 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -54,28 +54,22 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">Web App Development</h6>
-                <p>
-                  Web app development combines HTML, CSS, and JavaScript with
-                  backend languages to create interactive, responsive
-                  applications. Focused on user experience and scalability, it
-                  evolves with technological advancements, driving innovation in
-                  accessible, efficient global web solutions.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/web"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore Web development
-                  </Link>
-                </div>
+                <Link href="/service/web">
+                  <h6 className="s-22 w-700">Web App Development</h6>
+                  <p>
+                    Web app development combines HTML, CSS, and JavaScript with
+                    backend languages to create interactive, responsive
+                    applications. Focused on user experience and scalability, it
+                    evolves with technological advancements, driving innovation
+                    in accessible, efficient global web solutions.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
           {/* END FEATURE BOX #1 */}
           {/* FEATURE BOX #2 */}
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-2 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -88,28 +82,22 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">Software testing & QA</h6>
-                <p>
-                  Software testing and QA ensure that applications meet quality
-                  standards and function as intended, through systematic
-                  processes of verification, bug identification, and performance
-                  assessment to enhance product reliability.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/qa"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore QA
-                  </Link>
-                </div>
+                <Link href="/service/qa">
+                  <h6 className="s-22 w-700">Software testing & QA</h6>
+                  <p>
+                    Software testing and QA ensure that applications meet
+                    quality standards and function as intended, through
+                    systematic processes of verification, bug identification,
+                    and performance assessment to enhance product reliability.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
           <hr className="!mb-10 divider" />
           {/* END FEATURE BOX #2 */}
           {/* FEATURE BOX #3 */}
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-3 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -122,27 +110,21 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">Mobile App Development</h6>
-                <p>
-                  Mobile app development involves creating software for
-                  smartphones and tablets, focusing on optimized design,
-                  functionality, and user experience across diverse platforms
-                  and devices for on-the-go accessibility and performance.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/mobile"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore Mobile development
-                  </Link>
-                </div>
+                <Link href="/service/mobile">
+                  <h6 className="s-22 w-700">Mobile App Development</h6>
+                  <p>
+                    Mobile app development involves creating software for
+                    smartphones and tablets, focusing on optimized design,
+                    functionality, and user experience across diverse platforms
+                    and devices for on-the-go accessibility and performance.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
           {/* END FEATURE BOX #3 */}
           {/* FEATURE BOX #4 */}
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-4 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -155,21 +137,15 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">UI/UX Design</h6>
-                <p>
-                  UI/UX design merges visual design with user experience to
-                  create intuitive and attractive interfaces, focusing on
-                  {`user's`} ease, engagement, and satisfaction with a
-                  {`product's`} look and feel.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/uiux"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore UI/UX
-                  </Link>
-                </div>
+                <Link href="/service/uiux">
+                  <h6 className="s-22 w-700">UI/UX Design</h6>
+                  <p>
+                    UI/UX design merges visual design with user experience to
+                    create intuitive and attractive interfaces, focusing on
+                    {`user's`} ease, engagement, and satisfaction with a
+                    {`product's`} look and feel.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
@@ -178,7 +154,7 @@ const ServiceComponent = ({
 
           {/* END FEATURE BOX #4 */}
           {/* FEATURE BOX #5 */}
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-5 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -191,27 +167,21 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">Devops</h6>
-                <p>
-                  DevOps integrates development and operations to improve
-                  collaboration, speed, and product quality, focusing on
-                  continuous integration, automated deployment, and quicker,
-                  more efficient delivery cycles in software development.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/devops"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore Devops
-                  </Link>
-                </div>
+                <Link href="/service/devops">
+                  <h6 className="s-22 w-700">Devops</h6>
+                  <p>
+                    DevOps integrates development and operations to improve
+                    collaboration, speed, and product quality, focusing on
+                    continuous integration, automated deployment, and quicker,
+                    more efficient delivery cycles in software development.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
           {/* END FEATURE BOX #5 */}
           {/* FEATURE BOX #6 */}
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-4 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -224,27 +194,21 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">Digital Art</h6>
-                <p>
-                  Digital art encompasses creating visual content using
-                  technology, ranging from illustrations to animations,
-                  utilizing software and devices to craft, manipulate, and
-                  produce innovative artistic expressions in a digital format.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/art"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore Art
-                  </Link>
-                </div>
+                <Link href="/service/art">
+                  <h6 className="s-22 w-700">Digital Art</h6>
+                  <p>
+                    Digital art encompasses creating visual content using
+                    technology, ranging from illustrations to animations,
+                    utilizing software and devices to craft, manipulate, and
+                    produce innovative artistic expressions in a digital format.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
           <hr className="!mb-10 divider" />
 
-          <div className="col">
+           <div className="col  shadow-box">
             <div className="fbox-11 fb-4 wow fadeInUp">
               {/* Icon */}
               <div className="fbox-ico-wrap">
@@ -257,21 +221,16 @@ const ServiceComponent = ({
               {/* End Icon */}
               {/* Text */}
               <div className="fbox-txt">
-                <h6 className="s-22 w-700">AI/Machine Learning</h6>
-                <p>
-                  AI/Machine Learning involves algorithms and statistical models
-                  that enable computers to improve tasks with experience,
-                  focusing on prediction, automation, and decision-making in
-                  various fields from healthcare to finance.
-                </p>
-                <div className="pt-8 text-right">
-                  <Link
-                    href="/service/ai"
-                    className="btn r-04 btn--theme hover--theme"
-                  >
-                    Explore AI
-                  </Link>
-                </div>
+                <Link href="/service/ai">
+                  <h6 className="s-22 w-700">AI/Machine Learning</h6>
+                  <p>
+                    AI/Machine Learning involves algorithms and statistical
+                    models that enable computers to improve tasks with
+                    experience, focusing on prediction, automation, and
+                    decision-making in various fields from healthcare to
+                    finance.
+                  </p>
+                </Link>
               </div>
             </div>
           </div>
