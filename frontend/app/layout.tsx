be41b1ee409c86ec023a5a819e4e011440068d1e
Mohamed Ahmadian <m.ahmadian@oxolo.com>
2024-01-07 21:52:11 +0330

    add meta

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="robots" content="noindex, nofollow" />
         <link
           rel="shortcut icon"
           href="/images/favicon.ico"
