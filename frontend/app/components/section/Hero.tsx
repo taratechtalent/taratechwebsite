8671c9f415acccfe16d14b71f70e664087e68b0e
Mohamed Ahmadian <m.ahmadian@oxolo.com>
2024-01-23 20:27:28 +0330

    fix hero style

--- frontend/app/components/section/Hero.tsx
+++ frontend/app/components/section/Hero.tsx
@@ -57,7 +57,7 @@ export default function Hero() {
                         </span>
                         <div
                           className="s-26 w-700 color--black"
-                          style={{ marginLeft: "80px" }}
+                          style={{ marginLeft: "80px", height: "60px" }}
                           data-tooltip-id="my-tooltip-rounded"
                           data-tooltip-content="Click to see the Talent Value calculator"
                         >
